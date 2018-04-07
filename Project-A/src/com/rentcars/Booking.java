2b5d40884a1d6ff2aafef45c4d56087738e8151e
ENG.NAIF ALSHEHRI <IT676@NAIFs-MacBook-Pro-2.local>
2018-04-07 06:33:02 +0300

    Project A

--- Project-A/src/com/rentcars/Booking.java
+++ Project-A/src/com/rentcars/Booking.java
@@ -71,15 +71,15 @@ public abstract class Booking implements Payable {
 
     System.out.println("Customer Name: "+customer.getName());
     System.out.println("Customer Phone No: "+customer.getPhone());
+    if(this instanceof  VIPBooking)
+        System.out.println("Address : "+((VIPBooking)this).getAddress());
+
 
         System.out.println("Car Model : "+car.getModel());
         System.out.println("Car Color : "+car.getColor());
 
 
-    if(this instanceof  VIPBooking)
-        System.out.println("Address : "+((VIPBooking)this).getAddress());
-
-    System.out.println("Service Type : "+getServiceType());//polymorphism
+        System.out.println("Service Type : "+getServiceType());//polymorphism
         System.out.println();
 
 
